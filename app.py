3b64770c496c3a38f9d4704251ed66d9e36a213e
Mr Cthulhu <danielparucho@gmail.com>
2022-07-21 20:21:14 -0300

    fix app agualogic py mongo/cart

--- app.py
+++ app.py
@@ -1,132 +1,90 @@
-from flask import Flask, render_template, redirect
-from mongokit import Connection, Document, ObjectId
+from flask import Flask, render_template, redirect, session
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
+import random
 
+#############################
+# APP FLASK CONFIGURATION
 app = Flask(__name__)
-
-
-class Masvendidos(Document):
-    __database__ = 'agualogic'
-    __collection__ = 'masvendidos'
-    structure = {
-        'img': str,
-        'price': str,
-        'parrafo': str,
-    }
-
-
-class Productsfiltros(Document):
-    __database__ = 'agualogic'
-    __collection__ = 'productsfiltros'
-    structure = {
-        'img': str,
-        'price': str,
-        'parrafo': str,
-    }
-
-
-class Filtros(Document):
-    __database__ = 'agualogic'
-    __collection__ = 'categoryfiltros'
-    structure = {
-        'img': str,
-        'price': str,
-        'parrafo': str,
-    }
-
-
-class Accesorios(Document):
-    __database__ = 'agualogic'
-    __collection__ = 'categoryaccesorios'
-    structure = {
-        'img': str,
-        'price': str,
-        'parrafo': str,
-    }
-
-
-class Termos(Document):
-    __database__ = 'agualogic'
-    __collection__ = 'categorytermos'
-    structure = {
-        'img': str,
-        'price': str,
-        'parrafo': str,
-    }
-
-
-class Cart(Document):
-    __database__ = 'agualogic'
-    # La coleccion se crea automaticamente, si ya hay un registro de datos como en este caso
-    __collection__ = 'cart'
-    structure = {
-        'img': str,
-        'price': str,
-        'parrafo': str,
-    }
-
-
-db = Connection(host="localhost", port=27017)
-db.register([Masvendidos, Productsfiltros, Filtros, Accesorios, Termos, Cart])
+app.secret_key = ".."
+# DATABASE CONFIGURATION
+uri = os.environ.get('MONGO_DB_URI', "mongodb://127.0.0.1")
+client = MongoClient(uri)
+db = client.agualogic
+#############################
 
 
 @app.route("/")
 def home_view():
 
-    masvendidos = list(db.Productsfiltros.find({'top': "1"}))
-    return render_template("agualogic_home.html", masvendidos=masvendidos)
+    if not session.get('id'):
+        session['id'] = random.randint(12345, 99999)
+    masvendidos = list(db.productsfiltros.find({'top': "1"}))
+    return render_template("agualogic_home.html", masvendidos=masvendidos, id=session.get('id'))
 
 
 @app.route("/products")
 def products_view():
 
-    productsfiltros = list(db.Productsfiltros.find())
+    productsfiltros = list(db.productsfiltros.find())
     return render_template("agualogic_detalle.html", productsfiltros=productsfiltros)
 
 
 @app.route("/category/<category>")
 def catfiltros_view(category):
-    productsfiltros = list(db.Productsfiltros.find({'category': category}))
+    productsfiltros = list(db.productsfiltros.find({'category': category}))
     return render_template("agualogic_detalle.html", productsfiltros=productsfiltros)
 
 
 @app.route("/producto/detalle/<id>")
 def producto_view(id):
-    masvendidos = list(db.Productsfiltros.find({'top': "1"}))
+    masvendidos = list(db.productsfiltros.find({'top': "1"}))
     # Estructura para Redireccionar por categoria de un elemento con id.
-    productsfiltros = list(db.Productsfiltros.find({'_id': ObjectId(id)}))
+    productsfiltros = list(db.productsfiltros.find({'_id': ObjectId(id)}))
     return render_template("carrito.html", productsfiltros=productsfiltros, masvendidos=masvendidos)
 
 
 @app.route("/add/<id>")
 def add_product_to_cart(id):
     # Las variables de ruta como id siempre son un string inclusive los numeros
-    product = db.Productsfiltros.find_one({'_id': ObjectId(id)})
+    product = db.productsfiltros.find_one({'_id': ObjectId(id)})
 
     """
     La funcion ObjectId() convierte un string en un ObjectId -El ObjectID
     es un tipo de dato como un string, un entero o un booleano-
     """
+    if not session.get('id'):
+        return redirect('/')
 
-    newProduct = db.Cart()
-    newProduct['parrafo'] = product['parrafo']
-    newProduct['img'] = product['img']
-    newProduct['price'] = product['price']
-    newProduct.save()
+    user = session.get('id')
+
+    nuevo = db.cart()
+    nuevo['parrafo'] = product['parrafo']
+    nuevo['img'] = product['img']
+    nuevo['price'] = product['price']
+
+    nuevo['user_id'] = user
+
+    db.cart.insert_one(nuevo)
 
     return redirect('/cart')  # redirect me envia a la ruta de cart_view:
 
 
 @app.route("/cart")
 def cart_view():
-    productsfiltros = list(db.Cart.find())
-    masvendidos = list(db.Productsfiltros.find({'top': "1"}))
+    if not session.get('id'):
+        return redirect('/')
+
+    productsfiltros = list(db.cart.find())
+    masvendidos = list(db.productsfiltros.find({'top': "1"}))
     return render_template(
         "cart_detalle.html", productsfiltros=productsfiltros, masvendidos=masvendidos)
 
 
 @app.route("/checkout")
 def check_view():
-    cartproducts = list(db.Cart.find())
+    cartproducts = list(db.cart.find())
     # operación para calcular subtotales, sumar distintos valores.
     subtotal = 0
     for p in cartproducts:
